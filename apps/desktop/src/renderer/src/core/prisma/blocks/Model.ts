b4a6f4e01b9069c1207437eb932f5c82656e4719
Chaban Serhii <sergeitchaban@gmail.com>
2023-01-03 14:21:04 +0200

    Update Model.ts

--- apps/desktop/src/renderer/src/core/prisma/blocks/Model.ts
+++ apps/desktop/src/renderer/src/core/prisma/blocks/Model.ts
@@ -1,4 +1,23 @@
-import { ModelField } from '../fields/ModelField'
+import { BigIntField } from '../fields/scalarFields/BigIntField'
+import { BooleanField } from '../fields/scalarFields/BooleanField'
+import { BytesField } from '../fields/scalarFields/BytesField'
+import { DateTimeField } from '../fields/scalarFields/DateTimeField'
+import { DecimalField } from '../fields/scalarFields/DecimalField'
+import { FloatField } from '../fields/scalarFields/FloatField'
+import { IntField } from '../fields/scalarFields/IntField'
+import { JsonField } from '../fields/scalarFields/JsonField'
+import { StringField } from '../fields/scalarFields/StringField'
 import { Block } from './Block'
 
-export class Model extends Block<ModelField> {}
+export class Model extends Block<
+  | BigIntField
+  | StringField
+  | BooleanField
+  | BytesField
+  | DateTimeField
+  | DecimalField
+  | FloatField
+  | IntField
+  | JsonField
+  | StringField
+> {}
