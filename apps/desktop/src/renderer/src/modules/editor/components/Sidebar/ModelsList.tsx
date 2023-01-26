73eedca1c0f803e4467de77932a1d1c4e16371e3
Chaban Serhii <sergeitchaban@gmail.com>
2023-01-26 23:47:07 +0200

    Update ModelsList.tsx

--- apps/desktop/src/renderer/src/modules/editor/components/Sidebar/ModelsList.tsx
+++ apps/desktop/src/renderer/src/modules/editor/components/Sidebar/ModelsList.tsx
@@ -10,13 +10,15 @@ export const ModelsList = () => {
   const models = useList($modelIds, (id) => <ModelNavItem modelId={id} />)
 
   return (
-    <Stack pr="xs">
-      <TextInput
-        icon={<IconSearch size={14} />}
-        placeholder="Search models..."
-        disabled={!isEditorEnabled}
-      />
-      <CreateModelForm />
+    <Stack>
+      <Stack pr="xs">
+        <TextInput
+          icon={<IconSearch size={14} />}
+          placeholder="Search models..."
+          disabled={!isEditorEnabled}
+        />
+        <CreateModelForm />
+      </Stack>
       <ScrollArea offsetScrollbars h="100%">
         <Stack h="100%" spacing={5}>
           {models}
