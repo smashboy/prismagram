ead0f4c96ffd84d3f519494f2da3919703a976c6
Chaban Serhii <sergeitchaban@gmail.com>
2023-01-28 14:26:29 +0200

    Update RelationsManager.ts

--- packages/prisma-state/RelationsManager.ts
+++ packages/prisma-state/RelationsManager.ts
@@ -17,28 +17,43 @@ export class RelationsManager {
     const sourceName = uncapitalize(source.name)
     const targetName = uncapitalize(target.name)
 
-    if (type === RelationType.ONE_TO_ONE) {
-      let idField: ModelField | null = null
+    const targetNamePlural = `${targetName}s`
 
-      for (const field of [...source.fields.values()]) {
-        if (field.attributes.has('id')) {
-          idField = field
-          break
-        }
+    if (type === RelationType.MANY_TO_MANY) {
+      const sourceNamePlural = `${sourceName}s`
+
+      const targetRelationField = new RelationField(sourceNamePlural, source.name, target)
+      targetRelationField.setModifier('list')
+
+      const sourceRelationField = new RelationField(targetNamePlural, target.name, source)
+      sourceRelationField.setModifier('list')
+
+      target.addField(sourceNamePlural, targetRelationField)
+      source.addField(targetNamePlural, sourceRelationField)
+
+      return
+    }
+
+    let idField: ModelField | null = null
+
+    for (const field of [...source.fields.values()]) {
+      if (field.attributes.has('id')) {
+        idField = field
+        break
       }
+    }
 
-      if (!idField) return
+    if (!idField) return
 
-      const targetTypeFieldName = `${uncapitalize(source.name)}Id`
+    const targetTypeFieldName = `${uncapitalize(source.name)}Id`
 
-      const targetTypeField = createScalarFieldFromType(targetTypeFieldName, idField.type, target)
+    const targetTypeField = createScalarFieldFromType(targetTypeFieldName, idField.type, target)
 
-      if (!targetTypeField) return
+    if (!targetTypeField) return
 
+    if (type === RelationType.ONE_TO_ONE) {
       const sourceRelationField = new RelationField(targetName, target.name, source)
       sourceRelationField.setModifier('optional')
-
-      source.addField(targetName, sourceRelationField)
 
       const targetRelationField = new RelationField(sourceName, source.name, target)
       const targetRelationAttr = new RelationAttribute(targetRelationField)
@@ -49,22 +64,28 @@ export class RelationsManager {
       targetRelationField.attributes.set('relation', targetRelationAttr)
       targetTypeField.attributes.set('unique', new UniqueAttribute(targetTypeField))
 
+      source.addField(targetName, sourceRelationField)
       target.addField(sourceName, targetRelationField)
       target.addField(targetTypeFieldName, targetTypeField)
+
+      return
     }
 
-    if (type === RelationType.MANY_TO_MANY) {
-      const sourceNamePlural = `${sourceName}s`
-      const targetNamePlural = `${targetName}s`
-
-      const targetRelationField = new RelationField(sourceNamePlural, source.name, target)
-      targetRelationField.setModifier('list')
-
+    if (type === RelationType.ONE_TO_MANY) {
       const sourceRelationField = new RelationField(targetNamePlural, target.name, source)
       sourceRelationField.setModifier('list')
 
-      target.addField(sourceNamePlural, targetRelationField)
-      source.addField(targetNamePlural, sourceRelationField)
+      const targetRelationField = new RelationField(sourceName, source.name, source)
+      const targetRelationAttr = new RelationAttribute(targetRelationField)
+
+      targetRelationAttr.setFields([targetTypeField.name])
+      targetRelationAttr.setReferences([idField.name])
+
+      targetRelationField.attributes.set('relation', targetRelationAttr)
+
+      source.addField(targetName, sourceRelationField)
+      target.addField(sourceName, targetRelationField)
+      target.addField(targetTypeFieldName, targetTypeField)
     }
   }
 }
