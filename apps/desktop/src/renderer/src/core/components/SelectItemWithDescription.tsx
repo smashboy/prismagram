775cf25e14441ed0e56377680d3accfd507faefb
Chaban Serhii <sergeitchaban@gmail.com>
2023-01-31 18:47:21 +0200

    Update SelectItemWithDescription.tsx

--- apps/desktop/src/renderer/src/core/components/SelectItemWithDescription.tsx
+++ apps/desktop/src/renderer/src/core/components/SelectItemWithDescription.tsx
@@ -12,7 +12,7 @@ export const SelectItemWithDescription = forwardRef<HTMLDivElement, ItemProps>(
       <Group noWrap>
         <div>
           <Text size="sm">{value}</Text>
-          <Text size="xs" color="dimmed">
+          <Text size="xs" opacity={0.65}>
             {description}
           </Text>
         </div>
