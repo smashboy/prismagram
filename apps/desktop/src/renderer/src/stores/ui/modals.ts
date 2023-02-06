762cfdf0dce13ca00a9abfd1b2851244b66fa3ec
Chaban Serhii <sergeitchaban@gmail.com>
2023-02-06 17:19:28 +0200

    Update modals.ts

--- apps/desktop/src/renderer/src/stores/ui/modals.ts
+++ apps/desktop/src/renderer/src/stores/ui/modals.ts
@@ -1,21 +1,6 @@
-import { debounce } from 'patronum'
 import { createBooleanStore } from '@renderer/core/effector'
-import { combine } from 'effector'
 
 export const [$isOpenCreateProjectModal, toggleCreateProjectModalEvent] = createBooleanStore(false)
 export const [$isOpenSelectProjectModal, toggleSelectProjectModalEvent] = createBooleanStore(false)
 
 export const [$isOpenSettingsModal, toggleSettingsModalEvent] = createBooleanStore(false)
-
-export const [$isOpenDetailsViewInitial, toggleModelNodeSidebarEvent] = createBooleanStore(false)
-export const [$isOpenDetailsViewDebounced] = createBooleanStore(false)
-export const $isOpenDetailsView = combine({
-  isOpen: $isOpenDetailsViewInitial,
-  isOpenDebounced: $isOpenDetailsViewDebounced
-})
-
-debounce({
-  source: $isOpenDetailsViewInitial,
-  timeout: 200,
-  target: $isOpenDetailsViewDebounced
-})
