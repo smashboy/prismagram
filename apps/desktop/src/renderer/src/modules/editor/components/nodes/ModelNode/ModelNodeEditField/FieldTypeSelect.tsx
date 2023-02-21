d008b37ac41c91f5f9fdbef1cdb009df6c696035
Chaban Serhii <sergeitchaban@gmail.com>
2023-02-21 17:16:31 +0200

    Update FieldTypeSelect.tsx

--- apps/desktop/src/renderer/src/modules/editor/components/nodes/ModelNode/ModelNodeEditField/FieldTypeSelect.tsx
+++ apps/desktop/src/renderer/src/modules/editor/components/nodes/ModelNode/ModelNodeEditField/FieldTypeSelect.tsx
@@ -1,5 +1,5 @@
 import { Select } from '@mantine/core'
-import { $schemaState } from '@renderer/modules/editor/stores'
+import { $schemaState, setPrismaSchemaEvent } from '@renderer/modules/editor/stores'
 import { useStore } from 'effector-react'
 import { scalarOptionsArray } from 'prisma-state/constants'
 import { EnumModelField, RelationField, ScalarField } from 'prisma-state/_new/fields'
@@ -9,17 +9,24 @@ interface FieldTypeSelectProps {
 }
 
 export const FieldTypeSelect: React.FC<FieldTypeSelectProps> = ({ field }) => {
-  const { enumIds, modelIds } = useStore($schemaState)
+  const state = useStore($schemaState)
+
+  const { enumIds, modelIds } = state
+
+  const options = field.isRelation ? modelIds : [...scalarOptionsArray, ...enumIds]
 
   const handleSelect = (value: string | null) => {
     if (!value) return
+
+    field.setType(value)
+    setPrismaSchemaEvent(state._clone())
   }
 
   return (
     <Select
       label="Type"
       value={field.type}
-      data={[...scalarOptionsArray, ...modelIds, ...enumIds]}
+      data={options}
       onChange={handleSelect}
       sx={{ flex: 1 }}
       readOnly={field.isRelation}
