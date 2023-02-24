9b4d94907d74592a40e6e0eb601c48e56b52b64a
Chaban Serhii <sergeitchaban@gmail.com>
2023-02-24 12:29:11 +0200

    links update

--- apps/desktop/src/renderer/src/modules/settings/components/AboutAppSettings/AboutAppSettings.tsx
+++ apps/desktop/src/renderer/src/modules/settings/components/AboutAppSettings/AboutAppSettings.tsx
@@ -1,7 +1,7 @@
-import { ActionIcon, Button, Stack, Text } from '@mantine/core'
+import { Button, Group, Stack, Text } from '@mantine/core'
 import { appName, appVersion, invoke } from '@renderer/core/electron'
 import { APP_UPDATE_CHECK } from '@shared/common/configs/api'
-import { IconBrandGithub } from '@tabler/icons'
+import { IconBrandGithub, IconBug, IconEdit } from '@tabler/icons'
 import { capitalize } from 'lodash'
 import { SettingsSection } from '../SettingsSection'
 import { SettingsSectionPaper } from '../SettingsSectionPaper'
@@ -21,14 +21,35 @@ export const AboutAppSettings = () => {
           </Stack>
         </SettingsSectionPaper>
         <SettingsSectionPaper title="Links">
-          <ActionIcon
-            href="https://github.com/smashboy/prismagram"
-            target="_blank"
-            rel="noopener noreferrer"
-            component="a"
-          >
-            <IconBrandGithub />
-          </ActionIcon>
+          <Group>
+            <Button
+              href="https://github.com/smashboy/prismagram"
+              target="_blank"
+              rel="noopener noreferrer"
+              component="a"
+              leftIcon={<IconBrandGithub />}
+            >
+              Source code
+            </Button>
+            <Button
+              href="https://github.com/smashboy/prismagram/issues/new?assignees=&labels=bug&template=bug_report.md&title="
+              target="_blank"
+              rel="noopener noreferrer"
+              component="a"
+              leftIcon={<IconBug />}
+            >
+              Report a bug
+            </Button>
+            <Button
+              href="https://github.com/smashboy/prismagram/issues/new?assignees=&labels=enhancement&template=feature_request.md&title="
+              target="_blank"
+              rel="noopener noreferrer"
+              component="a"
+              leftIcon={<IconEdit />}
+            >
+              Suggest a feature
+            </Button>
+          </Group>
         </SettingsSectionPaper>
       </Stack>
     </SettingsSection>
