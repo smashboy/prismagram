865b596740a02fdca9e07eb469288ded04a93a65
Chaban Serhii <sergeitchaban@gmail.com>
2023-03-08 12:09:05 +0200

    Update DiagramEditor.tsx

--- apps/desktop/src/renderer/src/modules/editor/components/DiagramEditor.tsx
+++ apps/desktop/src/renderer/src/modules/editor/components/DiagramEditor.tsx
@@ -1,13 +1,11 @@
 import { Background } from 'reactflow'
-
-import '../css/editor.css'
 import { NodesToolbar } from './NodesToolbar'
 import { EditorToolbar } from './EditorToolbar'
-
 import { CreateRelationModal } from './CreateRelationModal'
 import { CreateEnumFieldModal } from './CreateEnumFieldModal'
 import { SchemaSidebarEditor } from './SchemaSidebarEditor'
 import { DiagramEditorProvider } from './DiagramEditorProvider'
+import '../css/editor.css'
 
 export const DiagramEditor = () => {
   return (
